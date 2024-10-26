2ac176299c71b083a891e9f7a0f9c26027813718
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2024-10-27 00:35:32 +0200

    outsource FileChooser

--- src/main/java/dot/javaFX/controller/KassenbonMainController.java
+++ src/main/java/dot/javaFX/controller/KassenbonMainController.java
@@ -1,27 +1,14 @@
 package dot.javaFX.controller;
-
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.ResourceBundle;
-
 import dot.business.receipt.Receipt;
 import dot.business.receipt.ReceiptScanner;
 import dot.javaFX.objects.ReceiptsValuesTableRow;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class KassenbonMainController {
@@ -30,48 +17,57 @@ public class KassenbonMainController {
      private File receiptFile;
      private List<Receipt> receipts = new ArrayList<>();
      private ReceiptScanner receiptScanner = new ReceiptScanner();
-     Pane tableViewNode;
+     private VBox tableViewNode;
+     private VBox fileChooserNode;
 
      @FXML
-     Text filePathText = null;
+     VBox tableViewContainer = null;
 
+     @FXML
+     VBox fileChooserContainer = null;
 
-    @FXML
-    VBox tableViewContainer = null;
-    
      TableViewController tableViewController;
-  
+     FileChooserViewControler fileChooserViewController;
+
      public void setStage(Stage stage) {
           this.stage = stage;
      }
 
-     private void setReceiptFile(File file) {
+     public void setReceiptFile(File file) {
           this.receiptFile = file;
      }
 
-     @FXML
-     private void initialize(){
-          try{
-          FXMLLoader tableViewLoader = new FXMLLoader(getClass().getResource("../fxml/TableView.fxml"));
-          tableViewNode = tableViewLoader.load();
-          tableViewController = tableViewLoader.getController();
-          tableViewContainer.getChildren().add(tableViewNode);
-     }catch(IOException e){  e.printStackTrace();}
+     private void initTableViewConroller() {
+          try {
+               FXMLLoader tableViewLoader = new FXMLLoader(getClass().getResource("../fxml/TableView.fxml"));
+               tableViewNode = tableViewLoader.load();
+               tableViewController = tableViewLoader.getController();
+               tableViewContainer.getChildren().add(tableViewNode);
+          } catch (IOException e) {
+               e.printStackTrace();
+          }
      }
 
-     private File getFileFromFileChooser() {
-          FileChooser fileChooser = new FileChooser();
-          FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("*.jpg, *.png, *.tif",
-          new ArrayList<String>(Arrays.asList("*.jpg", "*.png", "*.tif")));
-          fileChooser.getExtensionFilters().add(extFilter);
-          return fileChooser.showOpenDialog(stage);
+     private void initFileChooserContorller() {
+          try {
+               FXMLLoader fileChooserViewLoader = new FXMLLoader(
+                         getClass().getResource("../fxml/FileChooserView.fxml"));
+               fileChooserNode = fileChooserViewLoader.load();
+               fileChooserViewController = fileChooserViewLoader.getController();
+               fileChooserContainer.getChildren().add(fileChooserNode);
+               fileChooserViewController.setMainController(this);
+               fileChooserViewController.setStage(stage);
+
+          } catch (IOException e) {
+               // TODO Auto-generated catch block
+               e.printStackTrace();
+          }
      }
 
      @FXML
-     public void handelSelectReceiptBtn() {
-          setReceiptFile(getFileFromFileChooser());
-          System.out.print(receiptFile.getAbsolutePath());
-          filePathText.setText(receiptFile.getAbsolutePath());
+     private void initialize() {
+          initTableViewConroller();
+          initFileChooserContorller();
      }
 
      @FXML
@@ -79,7 +75,7 @@ public class KassenbonMainController {
           receiptScanner.setReceiptImage(receiptFile);
           Receipt receipt = receiptScanner.scannReceipt();
           receipts.add(receipt);
-          tableViewController.addRow(new ReceiptsValuesTableRow(receipts.size() , receipt, "null"));      
+          tableViewController.addRow(new ReceiptsValuesTableRow(receipts.size(), receipt, "null"));
      }
 
      @FXML
