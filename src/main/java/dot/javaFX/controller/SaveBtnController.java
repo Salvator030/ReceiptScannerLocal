26c14d865f56349e26366fca07c09dbd69022a59
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2024-10-28 01:37:02 +0100

    create controller

---
+++ src/main/java/dot/javaFX/controller/SaveBtnController.java
@@ -0,0 +1,12 @@
+package dot.javaFX.controller;
+
+import javafx.fxml.FXML;
+
+public class SaveBtnController {
+
+    @FXML
+    public void handleSaveBtn() {
+        System.out.println("c");
+    }
+
+}
