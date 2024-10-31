be5709eaca7941d48a1faa40ed6bc9aad6428a0e
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2024-10-31 18:09:09 +0100

    outsourcing of all functions that take care of the formation of the file name and password to the FileHandler

--- src/main/java/dot/business/handler/FileHandler.java
+++ src/main/java/dot/business/handler/FileHandler.java
@@ -3,13 +3,20 @@ package dot.business.handler;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class FileHandler {
 
     private File inputFile;
     private File outputFolder;
-    private String outputFileName;
+    private final String defaultFileName = "Kassenbons-Abrechnung";
+    private String outputFileName =  defaultFileName;
     private String outputFileDate;
+    private Path fullOuputFilePath; 
     private final String outputFileType = ".xlsx";
     private final String tessDataPath = "src/main/resources/tessdata";
     private final File documentsDirectory = new File(
@@ -43,12 +50,31 @@ public class FileHandler {
         return documentsDirectory;
     }
 
-        public String getOutputFileName() {
+    public String getOutputFileName() {
         return outputFileName;
     }
 
-    public void setOutputFileNamePrefix(String outputFileName) {
+    
+    
+
+
+
+
+
+    public Path getFullOuputFilePath() {
+        return fullOuputFilePath;
+    }
+
+    public String getOutputFileDate() {
+        return outputFileDate;
+    }
+
+    public void setOutputFileName(String outputFileName) {
         this.outputFileName = outputFileName;
+    }
+
+    public void setOutputFileDate(String outputFileDate) {
+        this.outputFileDate = outputFileDate;
     }
 
     public static void writeToFile(String name, String res) {
@@ -67,6 +93,23 @@ public class FileHandler {
         }
     }
 
+      private String monthAndYearToString(Date date) {
+        DateFormat dataFormat = new SimpleDateFormat("MM-yyyy");
+        return dataFormat.format(date);
+    }
 
+    private String createFullFileName(String previx, String date) {
+        return previx + "-" + date + ".xlsx";
+    }
+
+    private String fileLocationToString(File folder, String fileName, String date) {
+        String path = folder.getAbsolutePath();
+        return path.substring(0, path.length() - 1) + createFullFileName(fileName, date);
+    }
+
+    private String getFileLocation(File folder, String fileName, String date) {
+        String fileLocation = fileLocationToString(folder, fileName, date);
+        return fileLocation;
+    }
 
 }
