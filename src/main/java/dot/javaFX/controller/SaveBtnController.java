8a6b30ea2c62653b3740efa19cc6aa6b47d45c69
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2024-11-01 02:40:57 +0100

    implement handelSaveBtn function

--- src/main/java/dot/javaFX/controller/SaveBtnController.java
+++ src/main/java/dot/javaFX/controller/SaveBtnController.java
@@ -4,15 +4,14 @@ import javafx.fxml.FXML;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
 
 public class SaveBtnController {
 
     private Stage stage;
     private KassenbonMainController mainController;
 
-    
-
-    
     public void setStage(Stage stage) {
         this.stage = stage;
     }
@@ -21,18 +20,21 @@ public class SaveBtnController {
         this.mainController = mainController;
     }
 
-    
-    @FXML
-    public void handleSaveBtn() {
+    private File directoryChooser() {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setTitle("Ablage Ordner der Ecxel Datei");
         directoryChooser.setInitialDirectory(mainController.getFileHandler().getDocumentsDirectory());
         File selectedDirectory = directoryChooser.showDialog(stage);
-        mainController.getFileHandler().setOutputFolder(selectedDirectory);
+        return selectedDirectory;
     }
 
-  
-
-
+    @FXML
+    public void handleSaveBtn() throws NumberFormatException, IOException, ParseException {
+        File directory = directoryChooser();
+        if (directory != null) {
+            mainController.getFileHandler().setOutputFolder(directory);
+            mainController.writeReceiptsToExcel();
+        }
+    }
 
 }
