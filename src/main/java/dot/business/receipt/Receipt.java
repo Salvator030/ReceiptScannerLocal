9d72223a9c9a409c86664d5f4fe61a252dfa3b79
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2024-11-02 18:10:27 +0100

    new constructor, acsept diveren String as Date and parse them.

--- src/main/java/dot/business/receipt/Receipt.java
+++ src/main/java/dot/business/receipt/Receipt.java
@@ -1,12 +1,19 @@
 package dot.business.receipt;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import javafx.scene.input.DataFormat;
 
 public class Receipt {
 
     private String shopName;
     private double summ;
     private Date date;
+    private final SimpleDateFormat formaterFullDate = new SimpleDateFormat("dd.MM.yyyy");;
+    private final SimpleDateFormat formaterMonthAndYear = new SimpleDateFormat("MM.yyyy");;
 
     public Receipt(Date date, String shopName, double summ) {
         this.shopName = shopName;
@@ -14,8 +21,19 @@ public class Receipt {
         this.date = date;
     }
 
-    public Receipt(Object object, Object object2) {
-        //TODO Auto-generated constructor stub
+    public Receipt(String date, String shopName, double summ) {
+        this.shopName = shopName;
+        this.summ = summ;
+        try {
+            if (date.matches("\\d{2}.\\d{4}")) {
+                this.date = formaterMonthAndYear.parse(date);
+            } else {
+                this.date = formaterFullDate.parse(date);
+            }
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     public String getShopName() {
@@ -36,6 +54,14 @@ public class Receipt {
 
     public Date getDate() {
         return date;
+    }
+
+    public String getDateFormatStringFullDate() {
+        return formaterFullDate.format(this.date);
+    }
+
+    public String getDateFormatStringMonthAndYear() {
+        return formaterMonthAndYear.format(this.date);
     }
 
     public void setDate(Date date) {
