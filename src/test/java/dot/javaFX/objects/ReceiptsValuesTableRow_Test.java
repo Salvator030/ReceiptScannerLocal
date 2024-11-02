6332730b7554df679797f07ab8be2d99285cbce4
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2024-11-02 14:26:48 +0100

    refactor test function

--- src/test/java/dot/javaFX/objects/ReceiptsValuesTableRow_Test.java
+++ src/test/java/dot/javaFX/objects/ReceiptsValuesTableRow_Test.java
@@ -15,10 +15,10 @@ public class ReceiptsValuesTableRow_Test {
     private ObservableList<ReceiptsValuesTableRow> receipList = FXCollections
             .observableArrayList();
 
-            String[] dates = {"22.02.2024","30.01.2024", "05.03.2024","10.12.2023"};
+            String[] dates = {"22.02.2024","30.01.2024", "05.03.2024","10.12.2023","06.2024"};
     private void setRows() {
         try {
-            ReceiptsValuesTableRow r1, r2, r3, r4;
+            ReceiptsValuesTableRow r1, r2, r3, r4, r5;
 
             r1 = new ReceiptsValuesTableRow(1,
                     new Receipt(new SimpleDateFormat("dd.MM.yyyy").parse(dates[0]), "Aldi", 10.00), "Lebensmittel");
@@ -28,10 +28,12 @@ public class ReceiptsValuesTableRow_Test {
                     "Buromaterial", 5.95);
             r4 = new ReceiptsValuesTableRow(4, new SimpleDateFormat("dd.MM.yyyy").parse(dates[3]), "Saturn",
                     "Sachmittel", 99.99);
+r5 = new ReceiptsValuesTableRow(dates[4]);
             receipList.add(r1);
             receipList.add(r2);
             receipList.add(r3);
             receipList.add(r4);
+            receipList.add(r5);
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
@@ -46,7 +48,7 @@ public class ReceiptsValuesTableRow_Test {
             for (ReceiptsValuesTableRow r : receipList){
                 output += r.getDate() + " ";
             }
-            String strB = dates[3] +" "+ dates[1] + " " + dates[0] + " " + dates[2] + " ";
+            String strB = dates[3] +" "+ dates[1] + " " + dates[0] + " " + dates[2] + " " + dates[4] + " ";
             assertEquals(strB , output);
     }
 
