478eac6d22ddfac3caa34eaad68c528b138705e6
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2024-11-02 20:16:13 +0100

    change Date typ from Date  to String

--- src/main/java/dot/business/receipt/Receipt.java
+++ src/main/java/dot/business/receipt/Receipt.java
@@ -9,33 +9,21 @@ import javafx.scene.input.DataFormat;
 
 public class Receipt {
 
+    private int indexInTable;
     private String shopName;
     private double summ;
-    private Date date;
+    private String date;
+    private String purpose;
     private final SimpleDateFormat formaterFullDate = new SimpleDateFormat("dd.MM.yyyy");;
     private final SimpleDateFormat formaterMonthAndYear = new SimpleDateFormat("MM.yyyy");;
 
-    public Receipt(Date date, String shopName, double summ) {
+    public Receipt(String date, String shopName, double summ) {
         this.shopName = shopName;
         this.summ = summ;
         this.date = date;
     }
 
-    public Receipt(String date, String shopName, double summ) {
-        this.shopName = shopName;
-        this.summ = summ;
-        try {
-            if (date.matches("\\d{2}.\\d{4}")) {
-                this.date = formaterMonthAndYear.parse(date);
-            } else {
-                this.date = formaterFullDate.parse(date);
-            }
-        } catch (ParseException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-
+ 
     public String getShopName() {
         return shopName;
     }
@@ -52,20 +40,34 @@ public class Receipt {
         this.summ = summ;
     }
 
-    public Date getDate() {
+    public String getDate() {
         return date;
     }
 
-    public String getDateFormatStringFullDate() {
-        return formaterFullDate.format(this.date);
-    }
-
-    public String getDateFormatStringMonthAndYear() {
-        return formaterMonthAndYear.format(this.date);
-    }
-
-    public void setDate(Date date) {
+    public void setDate(String date) {
         this.date = date;
     }
+
+
+    public int getIndexInTable() {
+        return indexInTable;
+    }
+
+
+    public void setIndexInTable(int indexInTable) {
+        this.indexInTable = indexInTable;
+    }
+
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+
+    
 
 }
