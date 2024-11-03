8035dbc74159139fee456a2ddd5373960f0421a5
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2024-11-03 11:53:27 +0100

    impl test

--- src/test/java/dot/business/excel/FastexcelHelper_Test.java
+++ src/test/java/dot/business/excel/FastexcelHelper_Test.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import dot.javaFX.objects.ReceiptsValuesTableRow;
@@ -16,18 +17,21 @@ public class FastexcelHelper_Test {
     FastexcelHelper fastexcelHelper = new FastexcelHelper();
 
     HashMap<String, ArrayList<ReceiptsValuesTableRow>> map = new HashMap<String, ArrayList<ReceiptsValuesTableRow>>();
+    List<ReceiptsValuesTableRow> rowList = new ArrayList<>(Arrays.asList(
+        new ReceiptsValuesTableRow("01.2024"),
+        new ReceiptsValuesTableRow("02.01.2024"),
+        new ReceiptsValuesTableRow("02.2024"),
+        new ReceiptsValuesTableRow("12.02.2024"),
+        new ReceiptsValuesTableRow("15.02.2024"),
+        new ReceiptsValuesTableRow("05.2024"),
+        new ReceiptsValuesTableRow("05.05.2024"),
+        new ReceiptsValuesTableRow("01.2025")));
+
+      
+    
 
     @Test
-    public void spliReceiptRowsListByDate_ListWithDiverentRows_CorectMap() {
-        List<ReceiptsValuesTableRow> rowList = new ArrayList<>(Arrays.asList(
-                new ReceiptsValuesTableRow("01.2024"),
-                new ReceiptsValuesTableRow("02.01.2024"),
-                new ReceiptsValuesTableRow("02.2024"),
-                new ReceiptsValuesTableRow("12.02.2024"),
-                new ReceiptsValuesTableRow("15.02.2024"),
-                new ReceiptsValuesTableRow("05.2024"),
-                new ReceiptsValuesTableRow("05.05.2024"),
-                new ReceiptsValuesTableRow("01.2025")));
+    public void spliReceiptRowsListByDate_ListWithDiverentRows_outputCorectMap() {
 
         ArrayList<ReceiptsValuesTableRow> rows = new ArrayList<ReceiptsValuesTableRow>(Arrays.asList(rowList.get(1)));
         map.put(rowList.get(0).getDate(), rows);
@@ -39,5 +43,34 @@ public class FastexcelHelper_Test {
 
         HashMap<String, List<ReceiptsValuesTableRow>> ouput = fastexcelHelper.spliReceiptRowsListByDate(rowList);
         assertEquals(map, ouput);
+    }
+
+    @Test
+    public void mergeData_toDiverntLists_outputCorectList(){
+        List<ReceiptsValuesTableRow> newList = new ArrayList<>(Arrays.asList(
+            new ReceiptsValuesTableRow("01.2024"),
+            new ReceiptsValuesTableRow("02.01.2024"),
+   
+            new ReceiptsValuesTableRow("03.2024"),
+            new ReceiptsValuesTableRow("16.03.2024"),
+            new ReceiptsValuesTableRow("30.03.2024"),
+            new ReceiptsValuesTableRow("05.2024"),
+            new ReceiptsValuesTableRow("15.05.2024")));
+
+            List<ReceiptsValuesTableRow> acceptet = new ArrayList<>(Arrays.asList(
+                new ReceiptsValuesTableRow("01.2024"),
+                new ReceiptsValuesTableRow("02.01.2024"),
+                new ReceiptsValuesTableRow("02.2024"),
+                new ReceiptsValuesTableRow("12.02.2024"),
+                new ReceiptsValuesTableRow("15.02.2024"),
+                new ReceiptsValuesTableRow("03.2024"),
+                new ReceiptsValuesTableRow("16.03.2024"),
+                new ReceiptsValuesTableRow("30.03.2024"),
+                new ReceiptsValuesTableRow("05.2024"),
+                new ReceiptsValuesTableRow("15.05.2024"),
+                new ReceiptsValuesTableRow("01.2025")));
+
+                fastexcelHelper.mergeData(newList, rowList);
+                assertEquals(acceptet, newList);
     }
 }
