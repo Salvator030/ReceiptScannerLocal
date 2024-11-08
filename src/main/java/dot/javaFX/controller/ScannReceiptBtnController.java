623676ea02c7a7656d034cde321a0c433cd2065f
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2024-11-08 01:25:24 +0100

    clean code

--- src/main/java/dot/javaFX/controller/ScannReceiptBtnController.java
+++ src/main/java/dot/javaFX/controller/ScannReceiptBtnController.java
@@ -26,38 +26,38 @@ public class ScannReceiptBtnController {
     @FXML
     public void handelScannReceiptBtn() {
 
- Task<Void> task1 = new Task<Void>() {  //create Task
+        Task<Void> task1 = new Task<Void>() { // create Task
             @Override
             protected Void call() throws Exception {
-                scannReceiptBtn.setVisible(false);
-               progressIndicator.setVisible(true); //progress indicator set visible
-                receiptScanner.setReceiptImage(mainController.getFileHandler().getInputFile());
-                Receipt receipt = receiptScanner.scannReceipt(receiptScanner.scanImage());
-                System.out.println("5");
-                mainController.addReceiptInList(receipt);
-                System.out.println("6");
-                mainController.addReceiptInTable(receipt);
-
+                try {
+                    scannReceiptBtn.setVisible(false);
+                    progressIndicator.setVisible(true); // progress indicator set visible
+                    receiptScanner.setReceiptImage(mainController.getFileHandler().getInputFile());
+                    Receipt receipt = receiptScanner.scannReceipt(receiptScanner.scanImage());
+                    mainController.addReceiptInList(receipt);
+                    mainController.addReceiptInTable(receipt);
+                } catch (Exception e) {
+                    for (StackTraceElement s : e.getStackTrace()) {
+                        System.err.println(s);
+                    }
+                }
                 return null;
             }
 
             @Override
             protected void succeeded() {
-                progressIndicator.setVisible(false); //progress indicator set not visible
+                progressIndicator.setVisible(false); // progress indicator set not visible
                 scannReceiptBtn.setVisible(true);
                 mainController.toggleScannReceiptBtnViewDisable();
                 mainController.toggleTableViewDisable();
                 mainController.clearFilePathText();
                 mainController.toggleSaveBtnDisable();
-               }
+            }
         };
 
-        Thread thread1 = new Thread(task1);  //assign Task into thread
+        Thread thread1 = new Thread(task1); // assign Task into thread
         thread1.start();
 
-
-
-      
     }
 
 }
