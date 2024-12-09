f2a4ae402cdd975358ab4fa34547459d336bea0d
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2024-12-09 19:01:50 +0100

    simplefy test

--- src/test/java/dot/business/ocrScanner/Tess_Test.java
+++ src/test/java/dot/business/ocrScanner/Tess_Test.java
@@ -1,6 +1,7 @@
 package dot.business.ocrScanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 
@@ -11,8 +12,26 @@ import dot.business.receipt.ExampleReceiptStrings;
 public class Tess_Test {
     private Tess tess = new Tess();
 
+    private File testFile = new File("src/test/resources/ocrScannerTestFile.jpg");
+
     @Test
-    public void test_TessNormal_withExampleReceiptString(){
-        assertEquals(ExampleReceiptStrings.getExampleReceiptString1(), tess.tess4jNormal(new File("D:/Develop/Repositorys/java_ocr_test/src/main/resources/testBon5.jpg")));
+    public void test_TessNormal_withExampleReceiptString() {
+         String expected = "*  * Kundenbeleg * *\n" +
+                "Blume2000 SE\n" +
+                "\n" +
+                "Filiale 1140\r\n" +
+                "\n" +
+                "Marzahner Promenade 1 A\n" +
+                "12679 Berlin\n" +
+                "\n" +
+                "Danke für Ihren Einkauf!\n";
+
+                String actual = tess.tess4jNormal(testFile);
+
+                assertTrue(actual != null);
+                assertTrue(actual.contains("Blume2000"));
+                assertTrue(actual.contains("Filiale 1140"));
+                assertTrue(actual.contains("Marzahner"));
+                assertTrue(actual.contains("Danke für Ihren Einkauf!"));
     }
 }
